c528017e3c25e337c0f720461e1598dc355a7853
Joseph <lovedayjoseph07@gmail.com>
2026-09-03 23:46:13 +0100

    Add files via upload

---
+++ .streamlit.py
@@ -0,0 +1,5 @@
+[theme]
+primaryColor="#0284c7"
+font="sans serif"
+# Leaving backgroundColor, secondaryBackgroundColor, and textColor unset
+# allows Streamlit to dynamically switch between light and dark modes cleanly.
